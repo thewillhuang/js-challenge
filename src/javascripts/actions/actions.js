a3579e292db35033465aa5f028d047b7b687d064
William Huang <will.h86@gmail.com>
2015-11-07 11:58:14 -0800

    added actions for sortBy

--- src/javascripts/actions/actions.js
+++ src/javascripts/actions/actions.js
@@ -4,6 +4,8 @@ export const UPDATE_CONTACT = 'UPDATE_CONTACT';
 export const DELETE_CONTACT = 'DELETE_CONTACT';
 export const FILTER = 'FILTER';
 export const SET_ADDRESSBOOK = 'SET_ADDRESSBOOK';
+export const SORT_ASC = 'SORT_ASC';
+export const SORT_DEC = 'SORT_DEC';
 
 // action creators
 export function addContact(contact) {
@@ -32,6 +34,20 @@ export function filterBy(query) {
   return {
     type: FILTER,
     query: query,
+  };
+}
+
+export function sortAscBy(key) {
+  return {
+    type: SORT_ASC,
+    key: key,
+  };
+}
+
+export function sortDecBy(key) {
+  return {
+    type: SORT_DEC,
+    key: key,
   };
 }
 
--- test/actions.js
+++ test/actions.js
@@ -1,25 +1,70 @@
 'use strict';
 import {expect} from 'chai';
-import {addContact, updateContact, deleteContact, filterBy, setAddressBook} from '../src/javascripts/actions/actions.js';
+import {
+  CREATE_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+  FILTER,
+  SET_ADDRESSBOOK,
+  SORT_ASC,
+  SORT_DEC,
+  addContact,
+  updateContact,
+  deleteContact,
+  filterBy,
+  sortAscBy,
+  sortDecBy,
+  setAddressBook} from '../src/javascripts/actions/actions.js';
 
 describe('actions', () => {
   it('should construct addContact action', () => {
-    expect(addContact({test: 1})).to.eql({type: 'CREATE_CONTACT', contact: {test: 1}});
+    expect(addContact({test: 1})).to.eql({
+      type: CREATE_CONTACT,
+      contact: {test: 1}},
+    );
   });
 
   it('should construct updateContact action', () => {
-    expect(updateContact(1, {test: 2})).to.eql({type: 'UPDATE_CONTACT', index: 1, contact: {test: 2}});
+    expect(updateContact(1, {test: 2})).to.eql({
+      type: UPDATE_CONTACT,
+      index: 1,
+      contact: {test: 2},
+    });
   });
 
   it('should construct deleteContact action', () => {
-    expect(deleteContact(1)).to.eql({type: 'DELETE_CONTACT', index: 1});
+    expect(deleteContact(1)).to.eql({
+      type: DELETE_CONTACT,
+      index: 1,
+    });
   });
 
   it('should construct filterBy action', () => {
-    expect(filterBy('william')).to.eql({type: 'FILTER', query: 'william'});
+    expect(filterBy('william')).to.eql({
+      type: FILTER,
+      query: 'william',
+    });
   });
 
-  it('should constructsetAddressBook action', () => {
-    expect(setAddressBook({test: 1, date: 2})).to.eql({type: 'SET_ADDRESSBOOK', payload: {test: 1, date: 2}});
+  it('should construct setAddressBook action', () => {
+    expect(setAddressBook({
+      test: 1,
+      date: 2,
+    })).to.eql({
+      type: SET_ADDRESSBOOK,
+      payload: {test: 1, date: 2},
+    });
+  });
+
+  it('should construct sortAscBy action', () => {
+    expect(sortAscBy('test')).to.eql({
+      type: SORT_ASC,
+      key: 'test'});
+  });
+
+  it('should construct sortDecBy action', () => {
+    expect(sortDecBy('test2')).to.eql({
+      type: SORT_DEC,
+      key: 'test2'});
   });
 });
