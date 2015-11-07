8845bfbebb7bd9d8ae3510c973823778c1d89128
William Huang <will.h86@gmail.com>
2015-11-07 13:31:41 -0800

    let filter keep original data

--- src/javascripts/reducers/reducers.js
+++ src/javascripts/reducers/reducers.js
@@ -1,5 +1,13 @@
 import {List, fromJS} from 'immutable';
-import {CREATE_CONTACT, UPDATE_CONTACT, DELETE_CONTACT, FILTER, SET_ADDRESSBOOK} from '../actions/actions.js';
+import {
+  CREATE_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+  FILTER,
+  SET_ADDRESSBOOK,
+  SORT_ASC,
+  SORT_DEC,
+} from '../actions/actions.js';
 export const INITIAL_STATE = new List();
 
 export function addressBookApp(state = INITIAL_STATE, action) {
@@ -14,13 +22,39 @@ export function addressBookApp(state = INITIAL_STATE, action) {
     return state.delete(action.index);
 
   case FILTER:
-    return state.filter((x) => {
-      // console.log('what is x', x.get('fName'));
-      return (x.get('fName') === action.query);
+    const filtered = state.filter((map) => {
+      return (map.get('fName') === action.query);
+    });
+
+    return filtered.map(v => {
+      return v.set('beforeFilter', state);
     });
 
   case SET_ADDRESSBOOK:
     return fromJS(action.payload);
+
+  case SORT_ASC:
+    return state.sort((a, b) => {
+      let value = 0;
+      if (a.get(action.key) > b.get(action.key)) {
+        value = 1;
+      } else {
+        value = -1;
+      }
+      return value;
+    });
+
+  case SORT_DEC:
+    const key = action.key;
+    return state.sort((a, b) => {
+      let value = 0;
+      if (a.get(key) > b.get(key)) {
+        value = -1;
+      } else {
+        value = 1;
+      }
+      return value;
+    });
 
   default:
     return state;
--- test/reducers.js
+++ test/reducers.js
@@ -5,32 +5,40 @@ import chaiImmutable from 'chai-immutable';
 import chai from 'chai';
 chai.use(chaiImmutable);
 import {INITIAL_STATE, addressBookApp} from '../src/javascripts/reducers/reducers.js';
-import {addContact, updateContact, deleteContact, filterBy, setAddressBook} from '../src/javascripts/actions/actions.js';
+import {
+  addContact,
+  updateContact,
+  deleteContact,
+  filterBy,
+  setAddressBook,
+  sortAscBy,
+  sortDecBy,
+} from '../src/javascripts/actions/actions.js';
 
 
 describe('reducers', () => {
   describe('addressBookApp\'s', () => {
     let state = INITIAL_STATE;
-    it('addContact action should cause reducer to return an updated state ', () => {
+    it('addContact action should add a contact to the current state', () => {
       state = addressBookApp(state, addContact({test: 1}));
       expect(state).to.equal(fromJS([
         {test: 1},
       ]));
     });
 
-    it('updateContact action should cause reducer to return an updated state', () => {
+    it('updateContact action should update a specified contact of the current state', () => {
       state = addressBookApp(state, updateContact(0, {test: 2}));
       expect(state).to.equal(fromJS([
         {test: 2},
       ]));
     });
 
-    it('deleteContact action should cause reducer to return an updated state', () => {
+    it('deleteContact action should delete a contact from the current state given an index', () => {
       state = addressBookApp(state, deleteContact(0));
       expect(state).to.equal(fromJS([]));
     });
 
-    it('setAddressBook action should cause reducer to return an updated state', () => {
+    it('setAddressBook action should reset the state to be as the same as the payload', () => {
       state = addressBookApp(state, setAddressBook([
         {test: 1, day: 'sunny', fName: 'william'},
         {test: 2, day: 'rainy'},
@@ -42,10 +50,40 @@ describe('reducers', () => {
       ]));
     });
 
-    it('filterBy action should cause reducer to return an updated state', () => {
+    it('filterBy action should filter the current state by name', () => {
       state = addressBookApp(state, filterBy('william'));
+      const expectedSignature = fromJS([
+        {
+          test: 1,
+          day: 'sunny',
+          fName: 'william',
+          beforeFilter: [
+            {test: 1, day: 'sunny', fName: 'william'},
+            {test: 2, day: 'rainy'},
+          ],
+        },
+      ]);
+      expect(state).to.equal(expectedSignature);
+    });
+
+    it('sortAscBy action should cause reducer to return an updated state', () => {
+      state = addressBookApp(state, setAddressBook([
+        {test: 1, day: 'sunny', fName: 'william'},
+        {test: 2, day: 'rainy', fName: 'leeroy'},
+      ]));
+
+      state = addressBookApp(state, sortAscBy('day'));
+      expect(state).to.equal(fromJS([
+        {test: 2, day: 'rainy', fName: 'leeroy'},
+        {test: 1, day: 'sunny', fName: 'william'},
+      ]));
+    });
+
+    it('sortDecBy action should cause reducer to return an updated state', () => {
+      state = addressBookApp(state, sortDecBy('day'));
       expect(state).to.equal(fromJS([
         {test: 1, day: 'sunny', fName: 'william'},
+        {test: 2, day: 'rainy', fName: 'leeroy'},
       ]));
     });
   });
